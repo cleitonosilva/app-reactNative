d60142cc4ab42dc3d391b49e451da935e24a6529
Cleiton Oliveira <cleiton.logistica@outlook.com>
2021-02-03 17:45:26 -0300

    Pagina principal

---
+++ screens/Principal.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { Text,  View } from 'react-native';
+
+export default function Principal() {
+
+    return (
+        <View>
+            <Text>
+                Entrei na tela Principal
+            </Text>
+        </View>
+)
+
+}
